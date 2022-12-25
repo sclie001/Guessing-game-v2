cd56dc021e9962fd5356231b74f0845de6c0fd4d
Stephanie Cliett <sclie001@fiu.edu>
2022-12-24 23:05:16 -0500

    added background color indication to reflect whether the user guessed high, low, or matched the lucky number

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,25 @@ const randNum = Math.floor(Math.random() * 20);
 
 const checkBtn = document.getElementById('check-btn');
 const userInput = document.getElementById('guess');
-const message = document.getElementById('guess-message');
+const message = document.getElementById('game-message');
+const main_background = document.getElementById('main');
 
-console.log(userInput);
+console.log(randNum);
 
 function checkButton(){
     console.log('checking number...')
     if(userInput.value == randNum){
         console.log('You guessed the right number!');
         message.textContent = 'You guessed the right number!';
+        main_background.style.backgroundColor = '#006600';
     }else if(userInput.value > randNum){
         console.log('You guessed too high');
         message.textContent = 'You guessed too high';
+        main_background.style.backgroundColor = '#cc0000';
     }else if(userInput.value < randNum){
         console.log('You guessed low');
         message.textContent = 'You guessed too low';
+        main_background.style.backgroundColor = '#cc7a00';
     }
 }
 
